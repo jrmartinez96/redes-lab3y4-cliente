d1b1a577a878e127e222ea68c1c2260938d2d1b5
estebancabreraa <esteban.cabrera.a@gmail.com>
2020-09-03 18:28:52 -0600

    dvr algorithm

--- src/algoritmos-ruteo/distance-vector-routing.js
+++ src/algoritmos-ruteo/distance-vector-routing.js
@@ -9,6 +9,8 @@
  */
 export const dvr = (socket, nodos, id, idEnviar, idOrigen, mensaje, onSendMessage, extra) => {
     extra = extra || null;
+
+    let aristas = extra.aristas;
     // Obtener nombre de los nodos
     let nombreOrigen = "";
     let nombreDestinoFinal = ""
@@ -21,22 +23,70 @@ export const dvr = (socket, nodos, id, idEnviar, idOrigen, mensaje, onSendMessag
         }
     })
 
-
-
+    let distances = {};
+    let parents = {};
+    let c;
 
     // TODO: Implementar algoritmo para decidir a quien enviarle el mensaje
-
-    socket.emit('send-message', {
-        idNodoDestino: idEnviar, // Id del nodo al que se le quiere mandar el mensaje dentro de la red (el intermedio)
-        idNodoOrigen: idOrigen, // Id del nodo origen
-        idNodoDestinoFinal: idEnviar, // Id del nodo destino final
-        mensaje: mensaje, // Mensaje que se le quiere enviar
-    })
+    if (extra.done === false){
 
 
-    // Despues de mandar el mensaje
-    onSendMessage(`Se envió el mensaje de origen ${nombreOrigen} hacia ${nombreDestinoFinal}`);
-}
+            for (let i = 0; i < nodos.length; i += 1) {
+                distances[nodos[i].id] = Infinity;
+                parents[nodos[i].id] = null;
+            }
+            distances[idOrigen] = 0;
+            
+            for (let i = 0; i < nodos.length - 1; i += 1) {
+                for (var j = 0; j < aristas.length; j += 1) {
+                    c = aristas[j];
+                    if (distances[c.from.id] + c.peso < distances[c.to.id]) {
+                        distances[c.to.id] = distances[c.from.id] + c.peso;
+                        parents[c.to.from] = c.to.id;
+                    }
+                }
+            }
+
+            for (let i = 0; i < aristas.length; i += 1) {
+                c = aristas[i];
+                if (distances[c.from.id] + c.peso < distances[c.to.id]) {
+                    onSendMessage('El grafo tiene un ciclo negativo.');
+                }
+            }
+            
+            parents.reverse();
+
+            socket.emit('send-message', {
+                idNodoDestino: parents[idOrigen], // Id del nodo al que se le quiere mandar el mensaje dentro de la red (el intermedio)
+                idNodoOrigen: idOrigen, // Id del nodo origen
+                idNodoDestinoFinal: idEnviar, // Id del nodo destino final
+                mensaje: mensaje, // Mensaje que se le quiere enviar
+                extra: {parents: parents, done: true, counter: 0},
+            })
+            // Despues de mandar el mensaje
+            onSendMessage(`Se envió el mensaje de origen ${idOrigen} hacia ${parents[idOrigen]}`);
+        } else {
+            if (extra.counter <= extra.parents.length){
+                let counter = extra.counter + 1;
+                socket.emit('send-message', {
+                    idNodoDestino: idEnviar, // Id del nodo al que se le quiere mandar el mensaje dentro de la red (el intermedio)
+                    idNodoOrigen: extra.parents[idOrigen], // Id del nodo origen
+                    idNodoDestinoFinal: idEnviar, // Id del nodo destino final
+                    mensaje: mensaje, // Mensaje que se le quiere enviar
+                    extra: {parents: parents, done: true, counter: counter},
+                })
+            }
+            // Despues de mandar el mensaje
+            onSendMessage(`Se envió el mensaje de origen ${idOrigen} hacia ${extra.parents[idOrigen]}`);
+        }
+    } 
+
+
+
+
+
+
+
 
 /**
  * parametro nodos viene en una lista con el siguiente formato:
